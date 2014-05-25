05eb3552c800b3a81dd257be555bba6a7c4d0649
Raghu Betina <git@rugb.com>
2014-05-25 18:47:57 -0400

    rails g starter:resource comment content:text photo_id:integer user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525224107) do
+ActiveRecord::Schema.define(version: 20140525224739) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "photo_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: true do |t|
     t.string   "image_url"
