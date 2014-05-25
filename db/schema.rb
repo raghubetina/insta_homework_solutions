6f892d164fe3065f2151d230cfcaeb2fe209439c
Raghu Betina <git@rugb.com>
2014-05-25 18:48:29 -0400

    rails g starter:resource favorite photo_id:integer user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525224739) do
+ActiveRecord::Schema.define(version: 20140525224810) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
+    t.integer  "photo_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorites", force: true do |t|
     t.integer  "photo_id"
     t.integer  "user_id"
     t.datetime "created_at"
