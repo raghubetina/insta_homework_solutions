f7e283be9d78739857fbc277d4c066138a249067
Raghu Betina <git@rugb.com>
2014-05-25 18:42:43 -0400

    rails g starter:resource photo image_url:string caption:text user_id:integer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Photo resource:
+  # CREATE
+  get('/photos/new', { :controller => 'photos', :action => 'new' })
+  get('/create_photo', { :controller => 'photos', :action => 'create' })
+
+  # READ
+  get('/photos', { :controller => 'photos', :action => 'index' })
+  get('/photos/:id', { :controller => 'photos', :action => 'show' })
+
+  # UPDATE
+  get('/photos/:id/edit', { :controller => 'photos', :action => 'edit' })
+  get('/update_photo/:id', { :controller => 'photos', :action => 'update' })
+
+  # DELETE
+  get('/delete_photo/:id', { :controller => 'photos', :action => 'destroy' })
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
