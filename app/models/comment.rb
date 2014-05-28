7978f0e06adc0243d35f1c04eadd5e1731b4a97e
Raghu Betina <git@rugb.com>
2014-05-28 00:32:04 -0500

    Refactored by adding another helpful association method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :photo
 
+  belongs_to :user
+
   validates :content, :presence => true, :uniqueness => { :scope => :user_id }
 
   validates :user_id, :presence => true, :uniqueness => { :scope => :photo_id }
