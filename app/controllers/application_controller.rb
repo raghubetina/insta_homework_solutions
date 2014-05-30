c2ec7cbae390887ae233e1e39a913edabfb4f142
Raghu Betina <git@rugb.com>
2014-05-30 18:40:39 -0500

    Added custom fields to Devise forms, added custom stylesheet to override Bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :avatar_url
+
+    devise_parameter_sanitizer.for(:account_update) << :username
+    devise_parameter_sanitizer.for(:account_update) << :avatar_url
+  end
 end
